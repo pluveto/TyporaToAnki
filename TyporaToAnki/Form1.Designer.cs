8d177a4cc1cfe662ab4b19d87edf628a9e5032bd
Pluveto <receding@protonmail.com>
2020-12-19 20:20:56 +0800

    update

---
+++ TyporaToAnki/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TyporaToAnki
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 13);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(181, 245);
+            this.inputBox.TabIndex = 0;
+            this.inputBox.Text = resources.GetString("inputBox.Text");
+            this.inputBox.DoubleClick += new System.EventHandler(this.inputBox_DoubleClick);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(199, 13);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(23, 245);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = ">";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(228, 13);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(181, 245);
+            this.outputBox.TabIndex = 0;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 270);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ TyporaToAnki/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TyporaToAnki
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            var pattern = @"(\$\$)((.|\n)+?)(\$\$)";
+            var pattern2 = @"\$(.+?)\$";
+
+            var input = inputBox.Text;
+            var output = Regex.Replace(input, pattern, "\\[$2\\]");
+
+            output = Regex.Replace(output, pattern2, "\\($1\\)");
+            outputBox.Text = output;
+            try
+            {
+                Clipboard.SetText(output);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_DoubleClick(object sender, EventArgs e)
+        {
+            inputBox.Text = Clipboard.GetText();
+            btnConvert_Click(sender, e);
+        }
+    }
+}
